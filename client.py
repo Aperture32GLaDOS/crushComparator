64b14f6ad834cedb0cb56066ab82120fd9e3aa3d
Eric Hinchliffe <ehinchliffe@proton.me>
2023-09-18 07:41:25 +0100

    Added basic threading on server side; framework for secret sharing

--- client.py
+++ client.py
@@ -1,14 +1,34 @@
 import transport
 import encryption
 import socket
+import json
 
 public = encryption.readRSAKeyFromFile("public.key")
 
 HOST = "127.0.0.1"
 PORT = 6969
 
+client_ahead = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_behind = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 AESKey = encryption.generateKey()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 sock.send(AESKey)
+while True:
+    received = transport.receiveDynamicData(sock, AESKey)
+    if received[0] == "update":
+        data = json.loads(received[-1].decode(received[1]))
+        if client_ahead is not None:
+            try:
+                client_ahead.bind((data["clientAhead"], 0))
+            except IndexError:
+                pass
+        if client_behind is not None:
+            try:
+                client_behind.bind((data["clientBehind"], 0))
+            except IndexError:
+                pass
+    else:
+        print(received[-1].decode(received[1]))
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import transport
 import encryption
 import socket
+import json
+import threading
 
 private = encryption.readRSAKeyFromFile("private.key")
 public = encryption.readRSAKeyFromFile("public.key")
@@ -10,9 +12,19 @@ class Client:
     def __init__(self, sock, AESKey):
         self.sock = sock
         self.AESKey = AESKey
+        self.mainThread = threading.Thread(target=(self.listen), daemon=True)
+        self.mainThread.start()
 
     def sendMessage(self, message):
         transport.sendDynamicData(message.encode("utf-8"), "text", "utf-8", self.sock, self.AESKey)
+
+    def sendUpdate(self, update):
+        transport.sendDynamicData(json.dumps(update).encode("utf-8"), "update", "utf-8", self.sock, self.AESKey)
+
+
+    def listen(self):
+        while True:
+            transport.receiveDynamicData(self.sock, self.AESKey)
 
 HOST = "0.0.0.0"
 PORT = 6969
@@ -24,3 +36,8 @@ while True:
     clientsock = sock.accept()[0]
     AESKey = clientsock.recv(32)
     clients.append(Client(clientsock, AESKey))
+    if len(clients) == 1:
+        pass
+    else:
+        clients[-2].sendUpdate({"clientAhead": clients[1].sock.getpeername()[0]})
+        clients[-1].sendUpdate({"clientBehind": clients[0].sock.getpeername()[0]})
