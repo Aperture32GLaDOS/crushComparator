4ea28129ec08ca3c22a1119c4524b47810966a1a
Eric Hinchliffe <ehinchliffe@proton.me>
2023-09-18 07:10:28 +0100

    Added basic message sending

--- client.py
+++ client.py
@@ -7,6 +7,8 @@ public = encryption.readRSAKeyFromFile("public.key")
 HOST = "127.0.0.1"
 PORT = 6969
 
+AESKey = encryption.generateKey()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
-print(sock.recv(3))
+sock.send(AESKey)
--- server.py
+++ server.py
@@ -4,6 +4,15 @@ import socket
 
 private = encryption.readRSAKeyFromFile("private.key")
 public = encryption.readRSAKeyFromFile("public.key")
+clients = []
+
+class Client:
+    def __init__(self, sock, AESKey):
+        self.sock = sock
+        self.AESKey = AESKey
+
+    def sendMessage(self, message):
+        transport.sendDynamicData(message.encode("utf-8"), "text", "utf-8", self.sock, self.AESKey)
 
 HOST = "0.0.0.0"
 PORT = 6969
@@ -11,5 +20,7 @@ PORT = 6969
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 sock.listen()
-client = sock.accept()[0]
-client.send("idk".encode())
+while True:
+    clientsock = sock.accept()[0]
+    AESKey = clientsock.recv(32)
+    clients.append(Client(clientsock, AESKey))
